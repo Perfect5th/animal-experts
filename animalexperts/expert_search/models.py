2595aa41e84b83f2e2db32920c25a629de9b7c7b
perfect5th <mitch.b45@gmail.com>
2017-04-05 20:41:55 -0700

    initial migration of expert_search app

--- animalexperts/expert_search/models.py
+++ animalexperts/expert_search/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class FieldCategory(models.Model):
+    # Browseable field of expertise categories
+    name = models.CharField(max_length=200)
+    code = models.SlugField()
+    
+    class Meta:
+        verbose_name = "Field of Expertise"
+        verbose_name_plural = "Fields of Expertise"
+
+
+class Expert(models.Model):
+    # Representation of an individal Animal Expert
+    title = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    affiliation = models.CharField(max_length=200, 
+        help_text='University/Institution/Business')
+    subjects = models.TextField()
+    fields = models.ManyToManyField(FieldCategory)
+    email = models.EmailField()
+    website = models.URLField()
+    description = models.TextField()
