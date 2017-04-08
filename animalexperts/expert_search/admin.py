effbb08a110b3c71913ad3296c1d75d4c5e3ff4c
perfect5th <mitch.b45@gmail.com>
2017-04-08 15:48:08 -0700

    add series of basic templates remove django admin from installed apps add basic unit tests for first few endpoints add requirements.txt

--- animalexperts/expert_search/admin.py
+++ animalexperts/expert_search/admin.py
@@ -1,14 +1,3 @@
 from django.contrib import admin
-from .models import Expert, FieldCategory
-
-
-admin.AdminSite.site_header = "Animal Experts Admin"
 
 # Register your models here.
-class ExpertAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(Expert, ExpertAdmin)
-
-class FieldCategoryAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(FieldCategory, FieldCategoryAdmin)
--- animalexperts/expert_search/tests.py
+++ animalexperts/expert_search/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+class URLTest(TestCase):
+    urls = ['/', '/?q=test+search']
+    
+    def test_search_routes(self):
+        for url in self.urls:
+            r = self.client.get(url)
+            self.assertEqual(r.status_code, 200)
