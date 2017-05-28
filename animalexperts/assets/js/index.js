ecd65c49a03448150558b75e8ca241a961d3abed
Mitchell Burton <mitch.b45@gmail.com>
2017-05-27 17:32:26 -0700

    add results section and select box

--- animalexperts/assets/js/index.js
+++ animalexperts/assets/js/index.js
@@ -1,25 +1,48 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Search = React.createClass ({
-  render: function() {
+class Search extends React.Component {
+  render() {
     return (
       <div>
-        <div className="search-prompt-text">
-          <p>
-            Find me someone who is an expert in
-          </p>
-        </div>
-        <div className="search-box">
-          <div className="search-wrap">
-            <div className="search-padding">
-              <input type="text" className="expert-search" />
+        <div className="search-prompt">
+          <div className="search-prompt-text">
+            <p>
+              Find me someone who is an expert in
+            </p>
+          </div>
+          <div className="search-box">
+            <div className="search-wrap">
+              <div className="search-padding">
+                <input type="text" className="expert-search" />
+              </div>
             </div>
           </div>
         </div>
+        <div className="results-box"></div>
+        <div className="select-prompt">
+          <div className="search-prompt-text">
+            <p>
+              Show me experts in the field of
+            </p>
+          </div>
+          <div className="search-box">
+            <div className="search-wrap">
+              <div className="search-padding">
+                <select className="expert-search">
+                  <option value="0"></option>
+                  <option value="1">Value 1</option>
+                  <option value="2">Value 2</option>
+                  <option value="3">Value 3</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="results-box"></div>
       </div>
     )
   }
-});
+};
 
 ReactDOM.render(<Search />, document.getElementById('app'));
