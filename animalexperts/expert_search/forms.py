71c3c976de85e6cb218f5c7a6f80e3469adb1239
Mitchell Burton <mitch.b45@gmail.com>
2017-06-10 17:10:10 -0700

    remove email field from form/serializer styles for login form, add expert form

--- animalexperts/expert_search/forms.py
+++ animalexperts/expert_search/forms.py
@@ -6,6 +6,5 @@ from .models import Expert
 class ExpertForm(ModelForm):
     class Meta:
         model = Expert
-        fields = ['title', 'first_name', 'last_name',
-            'affiliation', 'subjects', 'fields', 'email',
-            'website', 'description']
+        fields = ['title', 'first_name', 'last_name', 'affiliation', 'subjects',
+            'fields', 'website', 'description']
