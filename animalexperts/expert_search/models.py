d163a23f32437bfdec6208c3cff90c977a99eb4b
Mitchell Burton <mitch.b45@gmail.com>
2017-06-14 17:06:14 -0700

    further changes to back office contributor stuff

--- animalexperts/expert_search/models.py
+++ animalexperts/expert_search/models.py
@@ -5,21 +5,21 @@ class FieldCategory(models.Model):
     # Browseable field of expertise categories
     name = models.CharField(max_length=200)
     code = models.SlugField(unique=True)
-    
+
     def __str__(self):
         return self.name
-    
+
     class Meta:
         verbose_name = "Field of Expertise"
         verbose_name_plural = "Fields of Expertise"
 
 
 class Expert(models.Model):
-    # Representation of an individal Animal Expert
+    # Representation of an individual Animal Expert
     title = models.CharField(max_length=10)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    affiliation = models.CharField(max_length=200, 
+    affiliation = models.CharField(max_length=200,
         help_text='University/Institution/Business')
     subjects = models.TextField()
     fields = models.ManyToManyField(FieldCategory)
