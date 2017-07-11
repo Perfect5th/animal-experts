4cc4c4e9fa78a94184aaf13a9f0dd15f315579c8
Mitchell Burton <mitch.b45@gmail.com>
2017-07-10 19:14:15 -0700

    bump django patch version to 1.11.3

--- animalexperts/expert_search/tests.py
+++ animalexperts/expert_search/tests.py
@@ -6,7 +6,7 @@ from .models import Expert
 # Create your tests here.
 class URLTest(TestCase):
     urls = ['/', '/?q=test+search']
-    
+
     def test_search_routes(self):
         for url in self.urls:
             r = self.client.get(url)
@@ -15,28 +15,28 @@ class URLTest(TestCase):
 
 class ContributorTest(TestCase):
     fixtures = ['one_user.json']
-    
+
     def test_contributor_login_logout(self):
         r = self.client.get('/contribute/login/')
         self.assertEqual(r.status_code, 200)
-        
+
         r = self.client.post('/contribute/login/', data={'username': 'mitch',
             'password': 'superexample'})
         self.assertEqual(r.status_code, 302)
-        
+
         r = self.client.get('/contribute/logout/')
         self.assertEqual(r.status_code, 302)
-    
+
     def test_contributor_change_password(self):
         user = User.objects.get(pk=1)
         old_pass = user.password
-        
+
         self.assertTrue(self.client.login(username='mitch',
             password='superexample'))
-        
+
         r = self.client.get('/contribute/password_change/')
         self.assertEqual(r.status_code, 200)
-        
+
         data = {'old_password': 'superexample', 'new_password1':
             'superexample2', 'new_password2': 'superexample2'}
         r = self.client.post('/contribute/password_change/', data=data)
@@ -44,17 +44,17 @@ class ContributorTest(TestCase):
         user.refresh_from_db()
         new_pass = user.password
         self.assertNotEqual(old_pass, new_pass)
-        
+
         r = self.client.get('/contribute/password_change_done/')
         self.assertEqual(r.status_code, 200)
-    
+
     def test_contributor_add_expert(self):
         self.assertTrue(self.client.login(username='mitch',
             password='superexample'))
-        
+
         r = self.client.get('/contribute/')
         self.assertEqual(r.status_code, 200)
-        
+
         data = {
             'title': 'Ms.',
             'first_name': 'New',
@@ -66,6 +66,6 @@ class ContributorTest(TestCase):
             'website': 'http://example.com/new_expert/',
             'description': 'a new expert.'
         }
-        r = self.client.post('/contribute/', data=data)
+        r = self.client.post('/contribute/add_expert/', data=data)
         self.assertEqual(r.status_code, 302)
         self.assertTrue(Expert.objects.filter(pk=1))
