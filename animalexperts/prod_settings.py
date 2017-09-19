4f0e42f6fe809d4876ab88eca2f24c9401d72d52
perfect5th <mitch.b45@gmail.com>
2017-09-19 09:26:51 -0700

    configure db settings for heroku deploy

--- animalexperts/prod_settings.py
+++ animalexperts/prod_settings.py
@@ -16,3 +16,8 @@ DATABASES = {
 }
 
 INSTALLED_APPS += ['django.contrib.postgres']
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
